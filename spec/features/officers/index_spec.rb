71f05aa8e907de36d6c848b2d8e8e91d3882ca8c
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-06 23:28:19 -0400

    1. Add feature tests for arrests show page 2. Add feature tests for officers show page 3. Add feature tests for officers index page 4. Generate SimpleCov report

--- spec/features/officers/index_spec.rb
+++ spec/features/officers/index_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
-# For each parent table
-# As a visitor
-# When I visit '/parents'
-# Then I see the name of each parent record in the system
-
 RSpec.describe 'it shows all officers in system' do
   before do
     officer_1 = Officer.create!(id: 1, name: "Zac Brown", badge_number: 23884, under_investigation: true)
