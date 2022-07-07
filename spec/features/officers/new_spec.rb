e37470452dbe9e3d013b01693e1cf9fda2e6f58a
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-07 18:03:56 -0400

    update new view

---
+++ spec/features/officers/new_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'it shows information on an officer based on id' do
+  before do
+    visit "/officers/new"
+  end
+
+end
+
+# [ ] done
+# User Story 11, Parent Creation 
+# As a visitor
+# When I visit the Parent Index page
+# Then I see a link to create a new Parent record, "New Parent"
+# When I click this link
+# Then I am taken to '/parents/new' where I  see a form for a new parent record
+
+# When I fill out the form with a new parent's attributes:
+# And I click the button "Create Parent" to submit the form
+# Then a `POST` request is sent to the '/parents' route,
+# a new parent record is created,
+# and I am redirected to the Parent Index page where I see the new Parent displayed.
