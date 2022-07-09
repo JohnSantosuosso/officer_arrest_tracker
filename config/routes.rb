f83b467e599d165e6ecb7d6e52fe28d5c8184717
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-09 18:50:21 -0400

    Add test to arrest show spec file for delete button Add route to routes.rb for delete action on arrests id Add destroy action to arrests controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   get '/arrests', to: 'arrests#index'
   get '/arrests/:id', to: 'arrests#show'
+  delete '/arrests/:id', to: 'arrests#destroy'
   get '/arrests/:id/edit', to: 'arrests#edit'
   patch '/arrests/:id', to: 'arrests#update'
 
