5892734a953b18221f37632241a4b65864a9b175
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-09 15:47:38 -0400

    1. Add Test to sort arrests alphabetically to officer_arrests index 2. Add link_to to officer_arrests to sort arrests alphabetically when link is clicked 3. Add table class and div class to narrow down testing for sorted names 4. Add test to ensure names are sorted when link is clicked

--- spec/features/officer_arrests/index_spec.rb
+++ spec/features/officer_arrests/index_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "officer arrests index page" do
   before do
     @officer_1 = Officer.create!(id: 1, name: "Zac Brown", badge_number: 23884, under_investigation: true)
     @arrest_1 = @officer_1.arrests.create!(id: 1, name: "Ralph Waldo Emerson", age: 23, charge_description: "Alcohol", firearm: true, created_at:"2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:05:39 UTC", officer_id: 1)
-    @arrest_2 = @officer_1.arrests.create!(id: 2, name: "Steve Smith", age: 25, charge_description: "Drug", firearm: false, created_at: "2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:06:36 UTC", officer_id: 1)
+    @arrest_2 = @officer_1.arrests.create!(id: 2, name: "Hamburglar", age: 25, charge_description: "Drug", firearm: false, created_at: "2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:06:36 UTC", officer_id: 1)
 
     visit "/officers/#{@officer_1.id}/arrests"
   end
@@ -31,16 +31,28 @@ RSpec.describe "officer arrests index page" do
   end
 
   it "links to an arrests show page" do
-    click_on(@arrest_1.name) #click on is capybara method
-    expect(current_path).to eql("/arrests/#{@arrest_1.id}") #current path is capybara method
+    click_on(@arrest_1.name)
+    expect(current_path).to eql("/arrests/#{@arrest_1.id}")
   end
 
   it 'links to a page to create a new arrest associated with this officer' do
-    #save_and_open_page
     expect(page).to have_content("Create Arrest")
     click_link ("Create Arrest")
     expect(current_path).to eql("/officers/#{@officer_1.id}/arrests/new")
   end
+
+  it 'links to an option to sort the arrests alphabetically' do
+    expect(page).to have_content("Sort Arrests Alphabetically")
+    click_link ("Sort Arrests Alphabetically")
+    expect(current_path).to eql("/officers/#{@officer_1.id}/arrests")
+
+    within "#arrest_table" do
+      expect(page.all('.arrest_name')[0]).to have_content("Hamburglar")
+      expect(page.all('.arrest_name')[1]).to have_content("Ralph Waldo Emerson")
+    end
+  
+  end
+
 end
 
 
