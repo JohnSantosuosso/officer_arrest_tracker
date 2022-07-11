9eba9a198d6a4a9fae684c35e9e192a03a1745ba
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-10 20:26:04 -0400

    add model test for age threshold method

--- spec/models/arrest_spec.rb
+++ spec/models/arrest_spec.rb
@@ -27,16 +27,29 @@ RSpec.describe Arrest, type: :model do
       end
     end
 
-      describe '#alpha orders arrests' do
+    describe '#alpha orders arrests' do
       before :each do
         @officer_1 = Officer.create!(id: 1, name: "Zac Brown", badge_number: 23884, under_investigation: true)
         @arrest_1 = Arrest.create!(id: 1, name: "Ralph Waldo Emerson", age: 23, charge_description: "Alcohol", firearm: true, created_at:"2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:05:39 UTC", officer_id: 1)
         @arrest_2 = Arrest.create!(id: 2, name: "Hamburglar", age: 25, charge_description: "Drug", firearm: false, created_at: "2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:06:36 UTC", officer_id: 1)
       end
       
-      it 'determines which arrests have a true value for firearm' do
+      it 'puts the arrests in alphabetical order' do
           show_alpha_arrests = Arrest.alpha_ordered
           expect(show_alpha_arrests.to_a).to eql([@arrest_2, @arrest_1])
+      end
+    end
+
+    describe '#age threshold test' do
+      before :each do
+        @officer_1 = Officer.create!(id: 1, name: "Zac Brown", badge_number: 23884, under_investigation: true)
+        @arrest_1 = Arrest.create!(id: 1, name: "Ralph Waldo Emerson", age: 23, charge_description: "Alcohol", firearm: true, created_at:"2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:05:39 UTC", officer_id: 1)
+        @arrest_2 = Arrest.create!(id: 2, name: "Hamburglar", age: 25, charge_description: "Drug", firearm: false, created_at: "2022-07-06 02:05:39 UTC", updated_at:"2022-07-06 02:06:36 UTC", officer_id: 1)
+      end
+      
+      it 'returns the arrest with an age above a certain threshold' do
+          show_arrests_above_24 = Arrest.age_threshold(24)
+          expect(show_arrests_above_24.to_a).to eql([@arrest_2])
       end
     end
   end
